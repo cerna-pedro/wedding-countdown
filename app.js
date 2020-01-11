d015d14be593cf9c8ce038a7062224a23bd5c09e
Pedro Cerna <cerna0909@gmail.com>
2020-01-11 17:20:22 -0600

    Added wedding date

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // query selectors
-const timer = document.querySelector('.timer');
+const timer = document.querySelector(".timer");
 
 // event listener on page load
-document.addEventListener('DOMContentLoaded', startTimer);
+document.addEventListener("DOMContentLoaded", startTimer);
 
 // functions
 function startTimer() {
-  const weddingDate = new Date('10/10/2020');
-  console.log(weddingDate)
+  const weddingDate = new Date("10/10/2020 4:30 pm");
+  const months = weddingDate.getMonth();
+  const days = weddingDate.getDay();
+  const hours = weddingDate.getHours();
+  const minutes = weddingDate.getMinutes();
+  const seconds = weddingDate.getSeconds();
+  const now = new Date();
+  timer.textContent = now.getMilliseconds();
 
+  setInterval(() => {
+    const now = new Date();
+    timer.textContent = now.getMilliseconds();
+  }, 1000);
 }
