4ee4f3936f4382f074628042f74e207e1b3483a4
Pedro Cerna <cerna0909@gmail.com>
2020-01-11 16:35:19 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// query selectors
+const timer = document.querySelector('.timer');
+
+// event listener on page load
+document.addEventListener('DOMContentLoaded', startTimer);
+
+// functions
+function startTimer() {
+  const weddingDate = new Date('10/10/2020');
+  console.log(weddingDate)
+
+}
